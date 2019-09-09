0c6ee8d3b465b74dae97ca3abdd40302727005f2
Prab <lifearoundpss@gmail.com>
2019-09-09 18:16:42 +0100

    filter and CSV export

--- frontend/src/components/pages/index.js
+++ frontend/src/components/pages/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import Auth from '../../lib/Auth'
-import { format } from 'd3-format'
-import { RadarChart } from 'react-vis'
-
-
-const wideFormat = format('.3r')
+import { CSVLink, CSVDownload } from 'react-csv'
 class Show extends React.Component {
   constructor() {
     super()
@@ -13,16 +9,25 @@ class Show extends React.Component {
       cards: [],
       showGraph: false,
       boxes: [],
-      search: ''
-
+      search: '',
+      shownBoxes: [],
+      loaded: false
     }
     this.handleKeyUpChange = this.handleKeyUpChange.bind(this)
     this.dataParser = this.dataParser.bind(this)
+    this.pageFilter = this.pageFilter.bind(this)
   }
 
+  pageFilter() {
+    const regex = new RegExp(this.state.search, 'i')
+    const filtered = this.state.boxes.filter(box => {
+      return regex.test(box.title)
+    })
+    return filtered
+  }
   handleKeyUpChange(e) {
-    console.log(e.target.value)
     this.setState({ search: e.target.value })
+
   }
   componentDidMount() {
 
@@ -44,14 +49,18 @@ class Show extends React.Component {
         return { name: value.description, domain: [0, 1] }
       })
       let graphPlotData = {}
+      const regex = new RegExp('^(?:https?:)?(?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)', 'ig')
+      const regexNamed = new RegExp('^(?:https?:)?(?:\/\/)?(?<title>:[^@\n]+@)?(?:www\.)?([^:\/\n]+)', 'ig')
       graphData.labelAnnotations.forEach(item => graphPlotData[item.description] = item.score)
       graphPlotData = [graphPlotData]
       const box = {}
       box.id = id
       box.document = document
-      box.created_at = created_at
-      box.updated_at = updated_at
+      box.created_at = new Date(created_at).toDateString()
+      box.updated_at = new Date(updated_at).toDateString()
       box.url = url
+      box.title = url.match(regex)
+      box.fTitle = url.replace(regexNamed, '')
       box.domain = domain
       box.graphPlotData = graphPlotData
       box.graphData = graphData
@@ -71,15 +80,16 @@ class Show extends React.Component {
           <div className="field">
             <label className="label">search</label>
             <div className="control">
-              <input className="input" type="text" placeholder="paste URL here"
+              <input className="input" type="text" placeholder="search"
                 onKeyUp={this.handleKeyUpChange}
               />
+              {this.state.loaded && <CSVLink data={this.state.boxes.graphPlotData}>Download me</CSVLink>}
             </div>
           </div>
         </div>
         <section className="section">
           <div className="container">
-            {this.state.boxes.map(item =>
+            {this.pageFilter().map(item =>
               <div key={item.id} className="box">
                 <article className="media">
                   <div className="media-left">
@@ -88,8 +98,8 @@ class Show extends React.Component {
                     </figure>
                   </div>
                   <div className="content">
-                    <p><strong>{item.url}</strong></p>
-                    <p>{Date(item.created_at)}</p>
+                    <p><strong>{item.title[0]}</strong></p>
+                    <p>{item.created_at}</p>
                     <div className="input-tag">
                       <ul className="input-tag__tags">
                         {item.domain.map((tag, i) => (
@@ -100,71 +110,12 @@ class Show extends React.Component {
                       </ul>
                     </div>
                   </div>
-                  <nav className="level is-mobile">
-                    <div className="level-left">
-                      <a className="level-item" aria-label="reply">
-                        <span className="icon is-small">
-                          <i className="fas fa-reply" aria-hidden="true"></i>
-                        </span>
-                      </a>
-                      <a className="level-item" aria-label="retweet">
-                        <span className="icon is-small">
-                          <i className="fas fa-retweet" aria-hidden="true"></i>
-                        </span>
-                      </a>
-                      <a className="level-item" aria-label="like">
-                        <span className="icon is-small">
-                          <i className="fas fa-heart" aria-hidden="true"></i>
-                        </span>
-                      </a>
-                    </div>
-                  </nav>
-
                 </article>
-
-
               </div>
             )}
           </div>
         </section>
-        <div className="container is-fluid is-danger">
-
-          {this.state.showGraph &&
-            <RadarChart
-              data={this.state.graphPlotData}
-              tickFormat={t => wideFormat(t)}
-              startingAngle={0}
-              domains={this.state.domain}
-              width={500}
-              height={500}
-              margin={{ left: 100, right: 100, top: 100, bottom: 100 }}
-              colorType="literal"
-              style={{
-                axes: {
-                  line: {
-                    fillOpacity: 0.8,
-                    strokeWidth: 0.5,
-                    strokeOpacity: 0.8
-                  },
-                  ticks: {
-                    fillOpacity: 0,
-                    strokeOpacity: 0
-                  },
-                  text: {}
-                },
-                labels: {
-                  fontSize: 10
-                },
-                polygons: {
-                  strokeWidth: 1,
-                  strokeOpacity: 0.8,
-                  fillOpacity: 0.8
-                }
-              }
-              }
-            />}
-        </div>
-      </section >
+      </section>
     )
   }
 }
